a4a794f47ba53889e6b43c04261aee7011aa575c
hellbound <mariovparaschiv@gmail.com>
2020-09-02 10:04:50 +0100

    fix this

--- src/finalizers/statcord.js
+++ src/finalizers/statcord.js
@@ -2,6 +2,8 @@ const { Finalizer } = require('klasa');
 
 module.exports = class extends Finalizer {
    async run(msg, command) {
-      await this.client.statcord.postCommand(command.name, msg.author.id);
+      if (command.category !== 'Utility') {
+         await this.client.statcord.postCommand(command.name, msg.author.id);
+      }
    }
 };
