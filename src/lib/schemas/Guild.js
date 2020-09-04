e5699aa56a9553e50597a0f8a9f63f9511847a82
hellbound <mariovparaschiv@gmail.com>
2020-09-04 09:45:17 +0100

    minor fixes

--- src/lib/schemas/Guild.js
+++ src/lib/schemas/Guild.js
@@ -1,26 +1,15 @@
 const { Client } = require('@serenity/core');
 
-module.exports = Client.defaultGuildSchema
-   .add('ignores', 'any', { array: true })
-   .add('rewards', 'any', { array: true })
-   .add('multiplier', 'number', { default: 1.0 })
-   .add('rankup', (folder) =>
-      folder
-         .add('enabled', 'boolean', { default: true })
-         .add('type', 'string', { default: 'message' })
-         .add('text', 'string', { default: ':partying_face: Congratulations %member, you are now level %level!' })
-         .add('channel', 'string', { default: 'current' })
-   )
-   .add('levels', (folder) =>
-      folder
-         .add('ignores', 'any', { array: true })
-         .add('rewards', 'any', { array: true })
-         .add('multiplier', 'number', { default: 1.0 })
-         .add('rankup', (folder) =>
-            folder
-               .add('enabled', 'boolean', { default: true })
-               .add('type', 'string', { default: 'message' })
-               .add('text', 'string', { default: ':partying_face: Congratulations %member, you are now level %level!' })
-               .add('channel', 'string', { default: 'current' })
-         )
-   );
+module.exports = Client.defaultGuildSchema.add('levels', (folder) =>
+   folder
+      .add('ignores', 'any', { array: true })
+      .add('rewards', 'any', { array: true })
+      .add('multiplier', 'number', { default: 1.0 })
+      .add('rankup', (folder) =>
+         folder
+            .add('enabled', 'boolean', { default: true })
+            .add('type', 'string', { default: 'message' })
+            .add('text', 'string', { default: ':partying_face: Congratulations %member, you are now level %level!' })
+            .add('channel', 'string', { default: 'current' })
+      )
+);
