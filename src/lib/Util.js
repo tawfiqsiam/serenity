636f8a97113d00fcc765dd7a78e029eb9388f4a0
hellbound <mariovparaschiv@gmail.com>
2020-09-04 05:52:34 +0100

    add required object

--- src/lib/Util.js
+++ src/lib/Util.js
@@ -15,4 +15,35 @@ module.exports = class Util {
          pageLength
       };
    }
+   
+   static permissions = {
+      ADMINISTRATOR: 'Administrator',
+      VIEW_AUDIT_LOG: 'View audit log',
+      MANAGE_GUILD: 'Manage server',
+      MANAGE_ROLES: 'Manage roles',
+      MANAGE_CHANNELS: 'Manage channels',
+      KICK_MEMBERS: 'Kick members',
+      BAN_MEMBERS: 'Ban members',
+      CREATE_INSTANT_INVITE: 'Create invites',
+      CHANGE_NICKNAME: 'Change nickname',
+      MANAGE_NICKNAMES: 'Manage nicknames',
+      MANAGE_EMOJIS: 'Manage emojis',
+      MANAGE_WEBHOOKS: 'Manage webhooks',
+      VIEW_CHANNEL: 'Read messages',
+      SEND_MESSAGES: 'Send messages',
+      SEND_TTS_MESSAGES: 'Send TTS messages',
+      MANAGE_MESSAGES: 'Manage messages',
+      EMBED_LINKS: 'Embed links',
+      ATTACH_FILES: 'Attach files',
+      READ_MESSAGE_HISTORY: 'Read message history',
+      MENTION_EVERYONE: 'Mention everyone',
+      USE_EXTERNAL_EMOJIS: 'Use external emojis',
+      ADD_REACTIONS: 'Add reactions',
+      CONNECT: 'Connect',
+      SPEAK: 'Speak',
+      MUTE_MEMBERS: 'Mute members',
+      DEAFEN_MEMBERS: 'Deafen members',
+      MOVE_MEMBERS: 'Move members',
+      USE_VAD: 'Use voice activity',
+   }
 };
