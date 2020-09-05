8a4b9bc53bd8e18a70a2946a8671361d87b9abe1
hellbound <mariovparaschiv@gmail.com>
2020-09-05 13:18:51 +0100

    Update Guild.js

--- src/lib/schemas/Guild.js
+++ src/lib/schemas/Guild.js
@@ -7,7 +7,7 @@ module.exports = Client.defaultGuildSchema.add('levels', (folder) =>
       .add('multiplier', 'number', { default: 1.0 })
       .add('rankup', (folder) =>
          folder
-            .add('enabled', 'boolean', { default: false })
+            .add('enabled', 'boolean', { default: true })
             .add('type', 'string', { default: 'message' })
             .add('text', 'string', { default: ':partying_face: Congratulations %member, you are now level %level!' })
             .add('channel', 'string', { default: 'current' })
